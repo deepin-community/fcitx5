06c5de5fe8dfac8c8cc049a233b5cb9561b26f5d
xzl <xiangzelong@deepin.org>
2025-03-31 13:50:49 +0800

    feat: update fcitx5 to
    Fcitx Input Method Framework v5 (input modules dev files)
    
    Issue: https://github.com/deepin-community/sig-deepin-sysdev-team/issues/558
    Log: update repo

--- src/im/keyboard/isocodes.cpp
+++ src/im/keyboard/isocodes.cpp
@@ -7,8 +7,10 @@
 
 #include "isocodes.h"
 #include <cstring>
+#include <string>
 #include <json-c/json.h>
 #include "fcitx-utils/metastring.h"
+#include "fcitx-utils/misc.h"
 
 namespace fcitx {
 
@@ -112,7 +114,8 @@ public:
         if (!alpha2 || json_object_get_type(alpha2) != json_type_string) {
             return;
         }
-        std::string alpha_2_code, name;
+        std::string alpha_2_code;
+        std::string name;
         name.assign(json_object_get_string(nameObj),
                     json_object_get_string_len(nameObj));
         alpha_2_code.assign(json_object_get_string(alpha2),
